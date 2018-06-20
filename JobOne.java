586674598ce824efb144c6c9872218f8d3b89cc1
Michele Proverbio <proch92@gmail.com>
2018-06-20 21:19:50 +0200

    job 1 mapreduce

---
+++ JobOne.java
@@ -0,0 +1,119 @@
+package mapreduce.orderbycrimes;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Calendar;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.JobClient;
+import org.apache.hadoop.mapreduce.JobConf;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+// lista ordinata dei quartieri di londra per occorrenze di un determinato crimine negli ultimi 5 anni
+public class JobOne {
+
+	public static class FilterMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable occurrencies = new IntWritable();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] record = value.toString().split(",");
+
+			// filter records on year constraint
+			int year = Integer.parseInt(record[4]);
+			int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+			if (currentYear - year > 5) {
+				return;
+			}
+
+			//filter records on crime constraint
+			String crime = context.getConfiguration().get("crime");
+			if (!record[2].equals(crime)) {
+				return;
+			}
+
+			occurrencies.set(record[3]);
+
+			context.write(record[1], occurrencies);
+		}
+	}
+
+	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static class InverterMapper extends Mapper<Text, Text, IntWritable, Text> {
+		IntWritable value = new IntWritable();
+
+		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+			value.set(Int.parseInt(value));
+
+			context.write(value, key);
+		}
+	}
+
+	public static class DescendingIntComparator extends WritableComparator {
+
+		public DescendingIntComparator() {
+			super(IntWritable.class, true);
+		}
+
+		@SuppressWarnings("rawtypes")
+		@Override
+		public int compare(WritableComparable w1, WritableComparable w2) {
+			IntWritable key1 = (IntWritable) w1;
+			IntWritable key2 = (IntWritable) w2;          
+			return -1 * key1.compareTo(key2);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("crime", args[2]);
+
+		JobConf job1 = new JobConf(conf, JobOne.class);
+		job1.setJobName("first pass");
+		job1.setMapperClass(FilterMapper.class);
+		job1.setCombinerClass(IntSumReducer.class);
+		job1.setReducerClass(IntSumReducer.class);
+		job1.setOutputKeyClass(Text.class);
+		job1.setOutputValueClass(IntWritable.class);
+
+		FileInputFormat.addInputPath(job1, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job1, new Path("temp"));
+
+		JobClient.runJob(job1);
+
+
+		JobConf job2 = new JobConf(new Configuration(), JobOne.class);
+		job1.setJobName("second pass");
+		job2.setMapperClass(InverterMapper.class);
+		job2.setSortComparatorClass(DescendingIntComparator.class);
+		job2.setReducerClass(Reducer.class);
+		job2.setNumReduceTasks(1);
+		job2.setOutputKeyClass(IntWritable.class);
+		job2.setOutputValueClass(Text.class);
+
+		FileInputFormat.addInputPath(job2, new Path("temp"));
+		FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+
+		JobClient.runJob(job2);
+	}
+}
