3854e00d1add2b08282fbc470c1e4c1da7ac10d9
Michele Proverbio <proch92@gmail.com>
2018-06-22 18:45:00 +0200

    creating dataframe with schema

--- src/bigdata/jobone/job1.py
+++ src/bigdata/jobone/job1.py
@@ -1,9 +1,21 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.typer import StructType, StructField, IntegerType, StringType
+
+# lista ordinata dei quartieri di londra per occorrenze di un determinato crimine negli ultimi 5 anni
 
 if __name__ == '__main__':
 	spark = SparkSession.builder.appName("py1").getOrCreate()
 
-	lines = spark.read.csv("data/london.csv", header=False).cache()
-	output = lines.map()
+	schema = StructType([
+			StructField("code", StringType()),
+			StructField("neigh", StringType()),
+			StructField("crime", StringType()),
+			StructField("subcrime", StringType()),
+			StructField("occ", IntegerType()),
+			StructField("year", IntegerType()),
+			StructField("month", IntegerType())
+		])
 
-	print lines.count()
+	df = spark.read.csv("data/london.csv", header=False, schema=schema).cache()
+
+	print df.columns
