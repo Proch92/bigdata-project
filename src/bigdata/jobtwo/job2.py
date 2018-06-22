519b82971212defef5703ddf373508b693dfb037
Michele Proverbio <proch92@gmail.com>
2018-06-22 20:03:33 +0200

    fix non ascii char

--- src/bigdata/jobtwo/job2.py
+++ src/bigdata/jobtwo/job2.py
@@ -2,8 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
 from pyspark.sql.functions import desc, col
 
-# per ogni anno determinare i 3 quartieri con la media di crimini al giorno più alta
-
 if __name__ == '__main__':
 	spark = SparkSession.builder.appName("py1").getOrCreate()
 
