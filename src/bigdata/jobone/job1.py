aecb9478f913e668eea865ae0702d41a5c36678e
Michele Proverbio <proch92@gmail.com>
2018-06-22 16:23:53 +0000

    job1.py tested

--- src/bigdata/jobone/job1.py
+++ src/bigdata/jobone/job1.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 if __name__ == '__main__':
 	spark = SparkSession.builder.appName("py1").getOrCreate()
 
-	lines = spark.read.csv("hdfs:///home/proch92@gmail_com/data/london.csv", header=False)
+	lines = spark.read.csv("data/london.csv", header=False).cache()
+	output = lines.map()
 
 	print lines.count()
