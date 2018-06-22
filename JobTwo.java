e48c7459348d5b216d4e84e3a07b3ad0729525f3
Michele Proverbio <proch92@gmail.com>
2018-06-22 12:07:30 +0200

    map it's not map. it's called foreach

--- JobTwo.java
+++ JobTwo.java
@@ -79,7 +79,7 @@ public class JobTwo {
 			String results = StreamSupport.stream(values.spliterator(), false).
 												sorted((o1, o2) -> ((FloatWritable) o2.get(new IntWritable(1))).compareTo(((FloatWritable) o1.get(new IntWritable(1))))).
 												limit(3).
-												map((m) -> "(" + m.get(new IntWritable(0)).toString() + ", " + m.get(new IntWritable(1)).toString() + ")").
+												forEach((m) -> "(" + m.get(new IntWritable(0)).toString() + ", " + m.get(new IntWritable(1)).toString() + ")").
 												collect(Collectors.joining(" "));
 
 			context.write(key, new Text(results));
